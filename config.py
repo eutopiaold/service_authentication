e371397f5ec368e52cb6c2f82a340cbef2420826
William Dry <wpbdry@gmail.com>
2019-03-25 17:06:31 +0100

    elephantsql database, mail service on aws

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ product_name = "Cube"
 
 # Edit the below variables to match correct database settings
 db_type = 'postgresql'
-db_user = 'postgres'
+db_user = 'dwokggtw'
 db_password = secret.database_password
-db_url = 'localhost'
+db_url = 'balarama.db.elephantsql.com'
 db_port = '5432'
-db_name = 'eut-auth'
+db_name = 'dwokggtw'
 
 db_connection_string = "%s://%s:%s@%s:%s/%s" % (db_type, db_user, db_password, db_url, db_port, db_name)
 
@@ -21,7 +21,7 @@ flask_port = 5000
 
 
 # Other microservices' IPs
-send_mail = "http://localhost:5001/send-mail"
+send_mail = "http://ec2-3-86-97-218.compute-1.amazonaws.com/send-mail"
 send_mail_password = secret.send_mail_password
 
 
--- secret.py
+++ secret.py
@@ -1,4 +1,5 @@
-database_password = 'jhL86&%$#f'
+#database_password = 'jhL86&%$#f'
+database_password = 'Cs_XIDZq5qF9WD9_OUVMdipMce7rF47n'
 
 send_mail_password = "skk3%8slSL@cm!!"
 
